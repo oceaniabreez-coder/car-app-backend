692143154e5bb2a89a3a87dd3e56468bcca62c0c
Ruwanga Karunarathna <ruwanga@Ruwangas-MacBook-Air.local>
2025-10-23 14:51:54 +1100

    change lower case

--- src/modules/car.service.ts
+++ src/modules/car.service.ts
@@ -13,7 +13,7 @@ export async function getCars(filter: CarFilter = {}, limit:number) {
       if (v === undefined || v === null ||v ===  "" || v === '') continue;
       if (typeof v === 'string') {
         console.log(k,v)
-        const t = v.trim().toLocaleLowerCase();
+        const t = v.trim().toLowerCase();
         if (t === '') continue;
         query = query.where(k as string, '==', t);
       } else {
